5f8a55e7ba7846d7b6e32b84f3852786fdc795f5
Vaibhav Borkar <vaibhavborkar3@gmail.com>
2023-07-07 11:28:58 +0530

    add assignment 2

--- scripts/run_assignment_2.py
+++ scripts/run_assignment_2.py
@@ -1,5 +1,6 @@
 import glob
 import logging
+import json
 import pandas as pd
 import sys
 
@@ -15,26 +16,42 @@ logger.setLevel("INFO")
 table_name = 'crime'
 input_csv = Path(f"data/{table_name}.csv")
 output_csv = Path(f"data/output_{table_name}.csv")
-column_names = [
-    "unique_key",
-    "case_number",
-    "date",
-    "block",
-    "primary_type",
-    "description",
-    "location_description",
-    "arrest",
-    "beat",
-    "district",
-    "ward",
-    "community_area",
-    "year",
-    "latitude",
-    "longitude",
-]
+table_schema_path = Path(f"schema/{table_name}.json")
 
 
-def extract():
+def get_columns(table_schema) -> list:
+    column_names = list(table_schema.keys())
+    return column_names
+
+def get_table_schema(table_schema_path) -> json:
+    f = open(table_schema_path)
+    data = json.load(f)
+    return data
+
+
+def create_table_sql(table_schema) -> str:
+    all_columns = []
+    pk_list = []
+    for columns, constraints_dict in table_schema.items():
+        column_details = [values for constraints, values in constraints_dict.items() if constraints != 'primary_key']
+        column_details = ' '.join(column_details)
+        all_columns.extend([columns + ' ' + column_details])
+
+        if 'primary_key' in constraints_dict:
+            pk_list.append(columns)
+
+    if pk_list:
+        pk_constraint = f", primary key ({', '.join(pk_list)})"
+        create_sql = ', '.join(all_columns) + pk_constraint
+    else:
+        create_sql = ', '.join(all_columns)
+
+    create_sql = f"create table {table_name} ({create_sql});"
+    logger.info("Table sql created")
+    return create_sql
+
+
+def extract(column_names):
     logger.info("Extracting data")
     data = pd.read_csv(input_csv, usecols = column_names)
     data['date'] = pd.to_datetime(data['date'], utc=True).dt.strftime('%Y-%m-%d %H:%M:%S')
@@ -59,17 +76,19 @@ def run_select_sql(cursor):
         sql_operations.select_data(cursor, select_query)
 
 
-def load_data(conn, cursor):
+def load_data(conn, cursor, column_names, table_schema):
     drop_table_sql = open("sql\drop_table.sql", "r").read()
-    create_table_sql = open("sql\create_table.sql", "r").read()
+    create_query = create_table_sql(table_schema)
     sql_operations.drop_table(conn, cursor, drop_table_sql)
-    sql_operations.create_table(conn, cursor, create_table_sql)
+    sql_operations.create_table(conn, cursor, create_query)
     sql_operations.insert_data(conn, cursor, table_name, column_names, output_csv)
 
 
 if __name__ == "__main__":
-    extract()
+    table_schema = get_table_schema(table_schema_path)
+    column_names = get_columns(table_schema)
+    extract(column_names)
     conn, cursor = connect_to_database()
-    load_data(conn, cursor)
+    load_data(conn, cursor, column_names, table_schema)
     run_select_sql(cursor)
     postgres_operations.close_connection(conn)
